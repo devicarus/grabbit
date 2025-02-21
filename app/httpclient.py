5e642215984ab6944892baac149a578e0ec58170
devicarus <devicarus@icloud.com>
2025-02-21 18:55:03 +0100

    ♻️ Changed HTTPClient exception handling

--- app/httpclient.py
+++ app/httpclient.py
@@ -1,11 +1,15 @@
 import requests
+from requests import Request
 from requests.models import Response
-from requests.exceptions import RequestException
+from requests.exceptions import ConnectionError, ReadTimeout
 from time import sleep
 from urllib.parse import urlparse
 from logging import Logger
 
 from app.utils import NullLogger
+
+class RetryLimitExceededException(Exception):
+    pass
 
 class HTTPClient:
     _headers: dict[str, str]
@@ -16,27 +20,26 @@ class HTTPClient:
         self._headers = headers if headers is not None else {}
         self._logger = logger if logger is not None else NullLogger()
 
-    def request(self, method: str, url: str, params: dict | None = None, max_retries: int = 3, **kwargs) -> Response:
+    def request(self, method: str, url: str, params: dict | None = None, max_retries: int = 3, timeout: int = 30, **kwargs) -> Response:
         retry_count = 0
         while retry_count < max_retries:
             try:
-                response = requests.request(method, url, headers=self._headers, params=params if params is not None else {}, timeout=30, **kwargs)
+                response = requests.request(method, url, headers=self._headers, params=params if params is not None else {}, timeout=timeout, **kwargs)
                 return response
-            except RequestException as e:
-                if isinstance(e, requests.exceptions.ConnectionError):
-                    if e.request and urlparse(e.request.url).hostname == "web.archive.org" and 'Errno 61' in str(e):
-                        self._logger.debug("Wayback Machine has overheated, cooling off for a minute...")
-                        retry_count += 1
-                        sleep(61)
-                        continue
-                if isinstance(e, requests.exceptions.ReadTimeout):
-                    self._logger.debug(f"Request timed out, retrying in {self._backoff_factor * (2 ** retry_count)} seconds")
+            except ConnectionError as e:
+                if isinstance(e.request, Request) and urlparse(e.request.url).hostname == "web.archive.org" and 'Errno 61' in str(e):
+                    self._logger.debug("Wayback Machine has overheated, cooling off for a minute...")
                     retry_count += 1
-                    sleep(self._backoff_factor * (2 ** retry_count))
+                    sleep(61)
                     continue
                 raise
+            except ReadTimeout:
+                self._logger.debug(f"Request timed out, retrying in {self._backoff_factor * (2 ** retry_count)} seconds")
+                retry_count += 1
+                sleep(self._backoff_factor * (2 ** retry_count))
+                continue
                 
-        raise Exception(f"Failed to fetch data from {url} after {max_retries} retries")
+        raise RetryLimitExceededException(f"Failed to fetch data from {url} after {max_retries} retries")
         
     def get(self, url: str, params: dict | None = None, **kwargs) -> Response:
         return self.request("GET", url, params if params is not None else {}, **kwargs)
